b18c8adb866e3601806f1cce0c489d34b852da7e
System Administrator <zaagagon@gmail.com>
2022-03-12 23:11:15 -0500

    compilando archivos en C++

--- funcion.cpp
+++ funcion.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int suma(int a,int b) {
+        return a +b;
+
+    } 
 int main() {
+   
 
     int total =20;
-    cout<<"suma"<<total;
+    cout<<"suma"<<total<<'\n';
+    cout<<suma(7,4);
 }
