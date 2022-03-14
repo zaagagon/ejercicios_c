5108cbca8f9c64898133f496628259b0f1ce163a
System Administrator <zaagagon@gmail.com>
2022-03-13 23:30:58 -0500

    cambios

--- Ej_switch.cpp
+++ Ej_switch.cpp
@@ -1,7 +1,26 @@
-//ejemplo switch en c++
+//ejemplo de menu
+//# include "iostream"
 #include <iostream>
+
 using namespace std;
 
-int main (){
+int main()
+{
+    cout << "Ingrese la Opción a ejecutar: ";
+    int opcion = 0;
+    cin >> opcion;
 
+    switch(opcion)
+    {
+        case 1: cout << "Usted ha seleccionado la opción 1";
+        break;
+        case 2: cout << "Usted ha seleccionado la opción 2";
+        break;
+        case 3: cout << "Usted ha seleccionado la opción 3";
+        break;
+        default: cout << "Usted ha ingresado una opción incorrecta";
+    }
+    // system("PAUSE"); //Solo ponla si no te da error
+
+    return 0;
 }
