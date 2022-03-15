d794fb50558ed6bdbe5b2b6fc1ac31c94a703d0c
System Administrator <zaagagon@gmail.com>
2022-03-14 23:11:00 -0500

    limpiar pantalla

--- menu3.cpp
+++ menu3.cpp
@@ -5,10 +5,24 @@ int suma(int a,int b){
     return a+b;
 }
 
+#ifdef _WIN32
+  #include<windows.h>
+#endif  
+//funcion limpiar pantalla
+void limpiar_pantalla()
+{
+  #ifdef _WIN32
+    system("cls");
+  #else
+    system("clear");
+  #endif
+}
+
 //funcion principal
 //g++ menu3.cpp -o menu3
 int main(){
     //prueba 
+     limpiar_pantalla();
     cout<<"** PRUEBA **";
     cout<<"\n";
     cout<<"Resultado de la operación : "<<suma(4,5)<<"\n";//imprime enter
